09f6847f7a610eeb4405b420883a45e6c5127374
lovinzada <lovinzada@gmail.com>
2022-10-06 10:57:11 -0300

    primeiro commit

---
+++ dbacademia.sql
@@ -0,0 +1,34 @@
+-- apagando o banco de dados caso exista
+drop database dbacademia;
+
+-- criar o banco de dados
+create database dbacademia;
+
+-- acessando o banco de dados
+use dbacademia;
+
+-- criando as tabelas do banco de dados
+create table tbfuncionarios(
+codigo int,
+nome varchar(100),
+email varchar(100),
+idade int,
+rg char(12),
+cpf char(14),
+logradouro varchar(100),
+numero char(10),
+bairro varchar(100),
+cidade varchar(50),
+estado varchar(100));
+
+create table tbusuarios(
+codigo int,
+nome varchar(20),
+senha varchar(10));
+
+-- visualizando as tabelas criadas
+show tables;
+
+-- visualizando a estrutura das tabelas
+desc tbfuncionarios;
+desc tbusuarios;
